fa1c11133b51296f35a607eb435d456d28725a7c
Lucas Baronio <lbaronio20@hotmail.com>
2023-10-16 12:39:54 -0300

    fix: add cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors');
 const { sendSlackMessage } = require('./slack');
 const app = express()
 const port = process.env.HTTP_PORT || 8080;
+
+app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
